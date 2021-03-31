a364fe3d8052ace6ba15e2d42d70b689cb1127bf
Anton Lutsenko <antnetbynet@yandex.com>
2021-03-31 23:56:20 +0300

    добавил Link в заголовок списка задач

--- src/components/Tasks/index.jsx
+++ src/components/Tasks/index.jsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 import AddTaskForm from "./AddTaskForm";
 import Task from "./Task";
@@ -33,25 +34,29 @@ export default function Tasks({
 
   return (
     <div className="tasks">
-      <h2 className="tasks__title" style={{ color: list.color.hex }}>
-        {list.name} <EditSvg onClick={editTitle} />
-      </h2>
+      <Link to={`/lists/${list.id}`}>
+        <h2 style={{ color: list.color.hex }} className="tasks__title">
+          {list.name}
+          <EditSvg onClick={editTitle} />
+        </h2>
+      </Link>
+
       <div className="tasks__items">
-        {!withoutEmpty && !list.tasks.length && <h2>Задачи отсутствуют</h2>}
+        {!withoutEmpty && list.tasks && !list.tasks.length && (
+          <h2>Задачи отсутствуют</h2>
+        )}
         {list.tasks &&
-          list.tasks.map(({ text, id, completed }) => (
+          list.tasks.map((task) => (
             <Task
-              key={id}
-              text={text}
-              id={id}
-              completed={completed}
+              key={task.id}
               list={list}
-              onRemove={onRemoveTask}
               onEdit={onEditTask}
-              onCompleted={onCompletedTask}
+              onRemove={onRemoveTask}
+              onComplete={onCompletedTask}
+              {...task}
             />
           ))}
-        <AddTaskForm key={list.id} list={list} handleAddTask={handleAddTask} />
+        <AddTaskForm key={list.id} list={list} onAddTask={handleAddTask} />
       </div>
     </div>
   );
